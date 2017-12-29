5c5470c5b5c2c85e843d7066bcd6708da84dfc15
tfb34 <britothalia212@gmail.com>
2017-12-29 15:12:17 -0500

    Display winner and add replay option

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+setTimeout(function(){
+    renderBoard();
+}, 200);
 
 //define gameBoard module
 var Gameboard = (function(){
@@ -111,16 +113,19 @@ let player2 = Player("Player 2", "O");
 // if valid, update array&board, check for win, next player's turn
 function renderBoard(){
 	for(let i = 0;i<9;i++){
-		document.getElementById("cell"+i).addEventListener("click", function(){playerMove(i)});//don't forget to change this function
+		document.getElementById("cell"+i).addEventListener("click", playerMove);//don't forget to change this function
 	}
 }
 
-function test(i){
-	console.log("cell"+i+" clicked");
+
+function test(){
+	console.log("worked");
+	console.log("!"+this.id[4]+"!");
 }
 
 // here is where we make sense of who is playing
-function playerMove(i){
+function playerMove(){
+	let i = this.id[4];
 	//check if space is occupued
 	let currPlayer;
 	if(gameController._currPlayer == 1){
@@ -140,21 +145,43 @@ function playerMove(i){
 		document.getElementById("cell"+i).append(p);
 		//update array
 		Gameboard.cells[i] = currPlayer.token;//maybe not neccessary unless playing with AI
-		
+		Gameboard.emptyCells--;
+
 		//check for win
 		if(gameController.check4Win(currPlayer)){
 			console.log("You won!");
-			//display on screen, replay, remove click event
-			document.getElementById('displayWinner').innerHTML = currPlayer.name+" Wins";
+			document.getElementById('displayWinner').innerHTML = currPlayer.name+" Wins! ";
+			createReplayOption();
+			disablePlayerMoves();
 		}else if(gameController.tie()){
 			console.log("Tie");
-			//display on screen, replay, remove click event
+			document.getElementById('displayWinner').innerHTML = "It's a tie! ";
+			createReplayOption();
 		}else{
 			gameController.nextPlayer();
 		}
 
 	}
 
+}
+
+function createReplayOption(){
+	let span=document.createElement('span');
+	span.innerHTML = "replay &#xf01e;";
+	span.className="fa";
+	span.setAttribute("onclick", "replay()");
+
+	document.getElementById('displayWinner').append(span);
+}
+
+function replay(){
+	window.location.reload(true);
+}
+
+function disablePlayerMoves(){
+	for(let i = 0;i<9;i++){
+		document.getElementById("cell"+i).removeEventListener("click", playerMove);//don't forget to change this function
+	}
 }
 
 
